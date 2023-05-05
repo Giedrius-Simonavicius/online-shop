fdb9882a2603cc8e167568a82fe89151e73b8732
Giedrius_Simonavicius <giedrius.simonavicius123@gmail.com>
2023-05-05 18:12:41 +0300

    t7

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -10,7 +10,7 @@ function HomePage() {
       </h1>
       <h2 className="underline">h2 text</h2>
       <p className="italic">
-        kazin ar veiks tailwind dabar ar ne? lyg ir vfdfvdeikikvgtasdafvsadds
+        kazin ar veiks tailwind dabar ar nge? lyg ir vfdfvdeikikvgtasdafvsadds
       </p>
     </div>
   );
