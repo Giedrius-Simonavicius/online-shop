9bd636967b4d938adb03c3501ba0a2812afff79c
Giedrius_Simonavicius <giedrius.simonavicius123@gmail.com>
2023-05-17 13:18:22 +0300

    new carousele

--- src/components/layout/Featuring.jsx
+++ src/components/layout/Featuring.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
-import Carousel from 'flat-carousel';
-import './Featuring.css';
+import Carousel from 'nuka-carousel';
 import feat1 from '.././../../public/images/featuring/featuring1.jpeg';
 import feat2 from '.././../../public/images/featuring/featuring2.jpeg';
 import feat3 from '.././../../public/images/featuring/featuring3.jpeg';
@@ -10,7 +9,7 @@ function Featuring() {
 
   return (
     <div className="container mx-auto max-w-90vw overflow-hidden">
-      <Carousel>
+      <Carousel autoplay={true} wrapAround={true}>
         {images.map((image, index) => (
           <img className="" key={index} src={image} alt={image} />
         ))}
@@ -18,4 +17,5 @@ function Featuring() {
     </div>
   );
 }
+
 export default Featuring;
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -3,35 +3,12 @@ import '../styles/style.css';
 import '../styles/App.css';
 import Featuring from '../components/layout/Featuring';
 import Products from '../components/homePageProducts/Products';
-import { db } from '../firebase/firebase';
-import { collection, getDocs } from 'firebase/firestore';
-import ToStorageA from '../components/ToStorageA';
 import { customPcs, desktops, laptops, monitors, posts } from '../data/data';
 import Brands from '../components/homePageProducts/Brands';
 import Posts from '../components/homePageProducts/Posts';
 
 function HomePage() {
   const [pcsArr, setPcsArr] = useState([]);
-
-  useEffect(() => {
-    async function getPcs() {
-      try {
-        const querySnapshot = await getDocs(collection(db, 'customPcs'));
-        const tempPcs = [];
-        querySnapshot.forEach((doc) => {
-          tempPcs.push({
-            uid: doc.id,
-            ...doc.data(),
-          });
-        });
-        setPcsArr(tempPcs);
-      } catch (error) {
-        console.log('error');
-      }
-    }
-
-    getPcs();
-  }, []);
 
   return (
     <div className="">
@@ -42,7 +19,6 @@ function HomePage() {
       <Products products={monitors} />
       <Brands />
       <Posts posts={posts} />
-      {/* <ToStorageA /> */}
     </div>
   );
 }
