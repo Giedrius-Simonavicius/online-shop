14af819ed21e3fca4a153c699cb60f5191bcb220
Giedrius_Simonavicius <giedrius.simonavicius123@gmail.com>
2023-05-22 15:33:45 +0300

    filter/delete

--- src/pages/AllProducts.jsx
+++ src/pages/AllProducts.jsx
@@ -6,7 +6,7 @@ import { useAuthCtx } from '../store/AuthProvider';
 function AllProducts({ products }) {
   const [inStock, setInStock] = useState([]);
   const [stars, setStars] = useState([]);
-  const { filterArr } = useAuthCtx();
+  const { filterArr, setFilterArr } = useAuthCtx();
 
   useEffect(() => {
     setInStock(products.map((product) => product.inStock));
@@ -43,15 +43,56 @@ function AllProducts({ products }) {
 
   const filteredProducts = filterProducts();
 
+  const handleDeleteFilter = (index) => {
+    const updatedFilters = [...filterArr];
+    updatedFilters.splice(index, 1);
+    setFilterArr(updatedFilters);
+  };
+
+  const countCategory = (category) => {
+    return products.reduce((count, product) => {
+      if (product.category === category) {
+        return count + 1;
+      }
+      return count;
+    }, 0);
+  };
+
+  const desktopCount = countCategory('desktops');
+  const monitorCount = countCategory('monitors');
+  const laptopCount = countCategory('laptops');
+  const customCount = countCategory("custom PC's");
+
   return (
     <div className="container  mx-auto mb-12 mt-12 flex">
       <div>
         <Filter />
       </div>
       <div className="flex-col">
-        <div>
+        <div className="mb-3 flex gap-2 text-sm">
           {filterArr.map((fObj, index) => (
-            <span key={index}>{capitalizeFirstLetter(fObj)} </span>
+            <div className=" flex gap-1 border py-1 pl-5 pr-2" key={index}>
+              <p>{capitalizeFirstLetter(fObj)}</p>
+              {fObj === 'desktops' && (
+                <span className="font-light text-color5">{`(${desktopCount})`}</span>
+              )}
+              {fObj === 'monitors' && (
+                <span className="font-light text-color5">{`(${monitorCount})`}</span>
+              )}
+              {fObj === 'laptops' && (
+                <span className="font-light text-color5">{`(${laptopCount})`}</span>
+              )}
+              {fObj === "custom PC's" && (
+                <span className="font-light text-color5">{`(${customCount})`}</span>
+              )}
+              <button
+                className="ml-3"
+                onClick={() => handleDeleteFilter(index)}
+              >
+                {' '}
+                <img src="/public/icons/ui/deleteBtn.svg" alt="deleteBtn" />
+              </button>
+            </div>
           ))}
         </div>
         <div className="mx-auto flex flex-wrap">
